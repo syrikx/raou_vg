b1690be1b486465135c5c1ab723500e317dfaa82
syrikx <syrikx@gmail.com>
2025-05-22 12:27:25 +0900

    first

---
+++ android/app/src/main/kotlin/com/innoview/raou/raou/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.innoview.raou.raou
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:raou/app/app.dart';
+import 'package:raou/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
